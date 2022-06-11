f2d30c9e0b3f7b19e1dfb808da66c8f6431ad453
Ricardo Mejias <panswer.rjmm@gmail.com>
2022-06-11 17:50:19 -0400

    update: change title in list of pacient between without pacients and there're anything

--- proyectoUno/citas-react-P1/src/Components/ListadoPacientes.jsx
+++ proyectoUno/citas-react-P1/src/Components/ListadoPacientes.jsx
@@ -2,17 +2,35 @@ import Paciente from "./Paciente"
 
 const ListadoPacientes = ({ pacientes }) => {
   return (
+
     <div className="md:w-1/2 lg:w-3/5 md:h-screen overflow-y-scroll">
-      <h2 className="font-black text-3xl text-center">Listado de pacientes</h2>
-      <p className="text-xl ml-5 mb-10 text-center">
-        Administra tus {''}
-        <span className="text-indigo-600 font-bold">Pacientes y Citas</span>
-      </p>
-
       {
-        pacientes.map(paciente => <Paciente key={paciente.id} paciente={paciente} />)
-      }
+        pacientes && pacientes.length ? (
+          <>
+            <h2 className="font-black text-3xl text-center">Listado de pacientes</h2>
+            <p className="text-xl ml-5 mb-10 text-center">
+              Administra tus {''}
+              <span className="text-indigo-600 font-bold">Pacientes y Citas</span>
+            </p>
 
+            {
+              pacientes.map(paciente => <Paciente key={paciente.id} paciente={paciente} />)
+            }
+          </>
+        ) : (
+          <>
+            <h2 className="font-black text-3xl text-center">No hay pacientes</h2>
+            <p className="text-xl ml-5 mb-10 text-center">
+              Comienza agregando pacientes y {''}
+              <span className="text-indigo-600 font-bold">apareceran en este lugar</span>
+            </p>
+
+            {
+              pacientes.map(paciente => <Paciente key={paciente.id} paciente={paciente} />)
+            }
+          </>
+        )
+      }
     </div>
   )
 }
