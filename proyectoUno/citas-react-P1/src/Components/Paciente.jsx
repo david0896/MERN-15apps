a6e719192bc43ae912828d9fdb66f182781d9414
Ricardo Mejias <panswer.rjmm@gmail.com>
2022-06-11 18:37:11 -0400

    update: buttons edit and delete on pacient

--- proyectoUno/citas-react-P1/src/Components/Paciente.jsx
+++ proyectoUno/citas-react-P1/src/Components/Paciente.jsx
@@ -1,5 +1,5 @@
 const Paciente = ({ paciente }) => {
-    const {nombre,propietario, email, fecha, sintomas} = paciente;
+    const { nombre, propietario, email, fecha, sintomas } = paciente;
     return (
         <div className="m-3 bg-white shadow-md px-5 py-10 rounded-xl">
             <p className="font-bold mb-3 text-gray-700 uppercase">
@@ -36,6 +36,16 @@ const Paciente = ({ paciente }) => {
                     {sintomas}
                 </span>
             </p>
+            <div className="flex justify-between">
+                <button
+                    type="button"
+                    className="py-2 px-10 bg-indigo-600 hover:bg-indigo-700 text-white font-bold uppercase rounded-lg"
+                >Editar</button>
+                <button 
+                type="button"
+                className="py-2 px-10 bg-red-600 hover:bg-red-700 text-white font-bold uppercase rounded-lg"
+                >Eliminar</button>
+            </div>
         </div>
     )
 }
