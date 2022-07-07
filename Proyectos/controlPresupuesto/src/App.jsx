32214cea74270fc2b8be9c7423622e37e415b479
david0896 <davidvasquez0896@gmail.com>
2022-07-07 09:22:38 -0400

    Agregando control swipe

--- Proyectos/controlPresupuesto/src/App.jsx
+++ Proyectos/controlPresupuesto/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import logo from './logo.svg'
 import Header from './Components/Header'
 import ListadoGastos from './Components/ListadoGastos'
@@ -12,6 +12,14 @@ function App() {
   const [modal, setModal] = useState(false);
   const [animarModal, setAnimarModal] = useState(false);
   const [gastos, setGastos] = useState([]);
+  const [gastoEditar, setGastoEditar] = useState({});
+
+  useEffect(() => {
+      if(Object.keys(gastoEditar).length > 0){
+        handleNuevoGasto();
+      }
+  }, [gastoEditar])
+  
 
   const handleNuevoGasto = () => {
       setModal(true);
@@ -48,6 +56,7 @@ function App() {
             <main>
               <ListadoGastos
                   gastos={gastos}
+                  setGastoEditar={setGastoEditar}
               />
             </main>
             <div className='nuevo-gasto'>
--- Proyectos/controlPresupuesto/src/Components/Gasto.jsx
+++ Proyectos/controlPresupuesto/src/Components/Gasto.jsx
@@ -1,4 +1,14 @@
+import {
+    LeadingActions,
+    SwipeableList,
+    SwipeableListItem,
+    SwipeAction,
+    TrailingActions
+} from 'react-swipeable-list'
+import 'react-swipeable-list/dist/styles.css' 
+
 import {formatearFecha} from '../Helpers'
+
 import iconoAhorro from '../img/icono_ahorro.svg'
 import iconoCasa from '../img/icono_casa.svg'
 import iconoComida from '../img/icono_comida.svg'
@@ -18,28 +28,52 @@ const diccionarioIconos = {
     suscripciones : iconoSuscripciones
 }
 
-const Gasto = ({gasto}) => {
 
-    const {categoria, nombre, cantidad, id, fecha} = gasto
+const Gasto = ({gasto, setGastoEditar}) => {
+
+    const {categoria, nombre, cantidad, id, fecha} = gasto;
+
+    const leadingActions = ()=>(
+        <LeadingActions>
+            <SwipeAction onClick={()=>setGastoEditar(gasto)}>
+                Editar
+            </SwipeAction>
+        </LeadingActions>
+    )
+    
+    const trailingActions = ()=>(
+        <TrailingActions>
+            <SwipeAction onClick={()=>console.log('eliminar...')}>
+                Eliminar
+            </SwipeAction>
+        </TrailingActions>
+    )
 
     return (
-        <div className="gasto sombra">
-            <div className="contenido-gasto">
-                <img 
-                    src={diccionarioIconos[categoria]}
-                    alt={`Categoria ${categoria}`}
-                />
-                <div className="descripcion-gasto">
-                    <p className="categoria">{categoria}</p>
-                    <p className="nombre-gasto">{nombre}</p>
-                    <p className="fecha-gasto">
-                        Agregado el: {''}
-                        <span>{formatearFecha(fecha)}</span>
-                    </p>
+        <SwipeableList>
+            <SwipeableListItem
+                leadingActions={leadingActions()}
+                trailingActions={trailingActions()}
+            >
+                <div className="gasto sombra">
+                    <div className="contenido-gasto">
+                        <img 
+                            src={diccionarioIconos[categoria]}
+                            alt={`Categoria ${categoria}`}
+                        />
+                        <div className="descripcion-gasto">
+                            <p className="categoria">{categoria}</p>
+                            <p className="nombre-gasto">{nombre}</p>
+                            <p className="fecha-gasto">
+                                Agregado el: {''}
+                                <span>{formatearFecha(fecha)}</span>
+                            </p>
+                        </div>
+                    </div>
+                    <p className='cantidad-gasto'>${cantidad}</p>
                 </div>
-            </div>
-            <p className='cantidad-gasto'>${cantidad}</p>
-        </div>
+            </SwipeableListItem>
+        </SwipeableList>
     )
 }
 
