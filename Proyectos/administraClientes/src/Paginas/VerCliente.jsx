db942e9e1e667c1e5dcf70844d0c6c5102d47aa1
david0896 <davidvasquez0896@gmail.com>
2022-09-17 08:57:23 -0400

    Detectando cuando no hay resultados, añadiendo spinner de carga

--- Proyectos/administraClientes/src/Paginas/VerCliente.jsx
+++ Proyectos/administraClientes/src/Paginas/VerCliente.jsx
@@ -1,12 +1,81 @@
+import {useEffect, useState} from 'react'
 import {useParams} from 'react-router-dom'
+import Spinner from '../components/Spinner';
 
 const VerCliente = () => {
 
+    const [cliente, setCliente] = useState({});
+    const [cargando, setCargando] = useState(true);
+
     const {id} = useParams();
-    console.log(id)
+
+    useEffect(() => {
+
+        const obtenerClienteAPI = async () => {
+            try{
+                const url = `http://localhost:4000/clientes/${id}`;
+                const respuesta = await fetch(url);
+                const resultado = await respuesta.json();
+                setCliente(resultado);
+            }catch(e){
+                console.log(e);
+            }
+
+            //simular conexion lenta 
+            setTimeout(()=>{
+                setCargando(!cargando);
+            }, 200)
+            
+        }
+
+        obtenerClienteAPI();
+
+    }, [])
+    
 
     return (
-        <div>VerCliente</div>
+        
+        cargando ? <Spinner/> : Object.keys(cliente).length === 0 ? <p>No hay resultados</p> : (
+            <>
+                <h1
+                className=' font-black text-4xl text-blue-900'
+                >Ver cliente: {cliente.nombre}</h1>
+                <p 
+                    className='mt-3 '
+                >Información del cliente</p>
+
+                {cliente.nombre && (
+                    <p className='text-4xl text-gray-600 mt-10'>
+                        <span className=' uppercase font-bold text-gray-800'>Cliente: </span>
+                        {cliente.nombre}
+                    </p>
+                )}
+                {cliente.email && (
+                    <p className='text-2xl text-gray-600 mt-4'>
+                        <span className=' uppercase font-bold text-gray-800'>Email: </span>
+                        {cliente.email}
+                    </p>
+                )}            
+                {cliente.telefono && (
+                    <p className='text-2xl text-gray-600 mt-4'>
+                        <span className=' uppercase font-bold text-gray-800'>Telefono: </span>
+                        {cliente.telefono}
+                    </p>
+                )}            
+                {cliente.empresa && (
+                    <p className='text-2xl text-gray-600 mt-4'>
+                        <span className=' uppercase font-bold text-gray-800'>Empresa: </span>
+                        {cliente.empresa}
+                    </p>
+                )}            
+                {cliente.notas && (
+                    <p className='text-2xl text-gray-600 mt-4'>
+                        <span className=' uppercase font-bold text-gray-800'>Notas: </span>
+                        {cliente.notas}
+                    </p>
+                )}
+            </>
+        )
     )
 }
 
---
+++ Proyectos/administraClientes/src/components/Spinner.jsx
@@ -0,0 +1,16 @@
+import '../styles/Spinner.css'
+
+const Spinner = () => {
+  return (
+    <div className="sk-chase">
+        <div className="sk-chase-dot"></div>
+        <div className="sk-chase-dot"></div>
+        <div className="sk-chase-dot"></div>
+        <div className="sk-chase-dot"></div>
+        <div className="sk-chase-dot"></div>
+        <div className="sk-chase-dot"></div>
+    </div>
+  )
+}
+
+export default Spinner
